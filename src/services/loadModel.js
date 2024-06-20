f1c60b8fb4cc6e7731c2caf2b3c05d0566353a41
Fauzan alfa <127025601+FzanAlfa@users.noreply.github.com>
2024-06-20 20:27:21 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -3,18 +3,8 @@ const path = require('path');
 require('dotenv').config();
 
 async function loadModel1() {
-    const modelPath = path.resolve(__dirname, process.env.MODEL_URL1.replace('file://', ''));
-
-    try {
-        // Memuat model menggunakan loadLayersModel
-        const model = await tf.loadLayersModel(`file://${modelPath}`);
-        console.log("Model berhasil dimuat.");
-        return model;
-    } catch (error) {
-        console.error("Kesalahan saat memuat model:", error);
-    }
+    return tf.loadGraphModel(process.env.MODEL_URL2);
 }
-
 
 async function loadModel2() {
     return tf.loadGraphModel(process.env.MODEL_URL2);
