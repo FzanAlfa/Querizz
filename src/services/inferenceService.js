222b89a802a895650ba439161f5ebcdad11e0d0c
Fauzan alfa <127025601+FzanAlfa@users.noreply.github.com>
2024-06-20 20:26:45 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -2,23 +2,6 @@ const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs').promises; 
 const PDFParser = require('pdf-parse');
 const InputError = require('../exceptions/InputError');
-
-//async function predictFile(model, bufferdata) {
-//  try {
-//    // Pastikan data yang diberikan adalah dalam bentuk Buffer
-//    if (!Buffer.isBuffer(bufferdata)) {
-//      throw new Error('Input harus berupa buffer data');
-//    }
-//
-//    // Menggunakan pdf-parse untuk mengonversi PDF ke teks
-//    const data = await PDFParser(bufferdata);
-//    const summarize = data.text;
-//    return summarize;
-//  } catch (error) {
-//    console.error('Error saat memproses file:', error);
-//    throw error; // atau lakukan penanganan error yang sesuai
-//  }
-//}
  
 async function predictFile(model, bufferdata) {
   try {
@@ -51,17 +34,17 @@ function preprocessText(text) {
   // Contoh sederhana preprocessing: tokenisasi menjadi kata dan konversi ke indeks angka
   const words = text.split(' ');
   const maxLength = 100; // Panjang maksimal sequence
-  const vocab = {'this': 1, 'is': 2, 'an': 3, 'example': 4}; // Contoh kamus kata ke indeks
+  const vocab = {'this': 1, 'is': 2, 'an': 3, 'example': 4};
   const data = words.map(word => vocab[word.toLowerCase()] || 0);
 
   // Padding atau trimming
   if (data.length < maxLength) {
-    data.push(...Array(maxLength - data.length).fill(0)); // Padding dengan 0
+    data.push(...Array(maxLength - data.length).fill(0));
   } else {
-    data.length = maxLength; // Trimming jika lebih panjang dari maxLength
+    data.length = maxLength; 
   }
 
-  return [data]; // Kembalikan dalam bentuk array 2D (batch_size, sequence_length)
+  return [data]; 
 }
 
 
